82b30237312ae5b80d1c1cf6790f67e40ad3eea2
ToasterTom <tate.thomas5595@gmail.com>
2016-12-29 22:59:24 -0700

    Game Functioning

--- jQuery.js
+++ jQuery.js
@@ -7,19 +7,27 @@ $(document).ready(function () {
 
   // Saving the snakes cell width in a variable for easy control.
   var cw = 10;
-
-  var direction = "right"; //Sets default direction.
-
-  // Canvas Styling
-  // ctx.fillStyle = "white";
-  // ctx.fillRect(0,0,w,h);
-  // ctx.strokeStyle = "black";
-  // ctx.strokeRect(0,0,w,h);
+  var direction;
+  var food;
+  var score;
 
   // Snake
   var snakeArray; //An array of cells that make up the snake.
 
-  createSnake();
+  function init() {
+
+      direction = "right"; //Sets default direction.
+      createSnake();
+      createFood();
+      score = 0;
+
+      //Lets move the snake now using a timer which will trigger the paint function
+  //every 60ms
+      if(typeof gameLoop != "undefined") clearInterval(gameLoop);
+  gameLoop = setInterval(snakeStyle, 200);
+  }
+
+  init();
 
   function createSnake() {
     var length = 5;
@@ -33,10 +41,19 @@ $(document).ready(function () {
     };
   };
 
+  // Food
+  function createFood() {
+    food = {
+      x: Math.round(Math.random()*(w-cw)/cw),
+      y: Math.round(Math.random()*(h-cw)/cw),
+      //This will create a cell with x/y between 0-44
+		//Because there are 45(450/10) positions accross the rows and columns
+    }
+  }
+
   // Snake style
   function snakeStyle() {
-    for (var i = 0; i < snakeArray.length; i++) {
-      //To avoid the snake trail we need to paint the BG on every frame
+
       ctx.fillStyle = "white";
       ctx.fillRect(0,0,w,h);
       ctx.strokeStyle = "black";
@@ -55,21 +72,68 @@ $(document).ready(function () {
       else if (direction == 'up') newY--;
       else if (direction == 'down') newY++;
 
+      //This will restart the game if the snake hits the wall
+      if (newX == -1 || newX == w/cw || newY == -1 || newY == h/cw || checkCollision(newX, newY, snakeArray)) {
 
+        init();
+        //restarts game
+        return;
+      }
 
-      var tail = snakeArray.pop();//pops out the last cell.
-      tail.x = newX;
+      //Lets write the code to make the snake eat the food
+      //The logic is simple
+      //If the new head position matches with that of the food,
+      //Create a new head instead of moving the tail
+
+      if (newX == food.x && newY == food.y) {
+        var tail = {x: newX, y: newY};
+        score++;
+        //create new food
+        createFood();
+      }
+      else {
+        {
+          var tail = snakeArray.pop();//pops out the last cell.
+          tail.x = newX;
+          tail.y = newY;
+        }
+      }
+
       snakeArray.unshift(tail);//puts back the tail as the first cell
+
+    for (var i = 0; i < snakeArray.length; i++) {
 
       var color = snakeArray[i];
       ctx.fillStyle = "#F4743C";
-      //Seting rectangles 10px by 10px.
       ctx.fillRect(color.x*cw, color.y*cw, cw, cw);
       ctx.strokeStyle = "white";
       ctx.strokeRect(color.x*cw, color.y*cw, cw, cw);
     };
+    //food style
+    styleCells(food.x, food.y);
+    var scoreText = "Score: " + score;
+    ctx.fillText(scoreText, 5, h-5);
   };
   snakeStyle();
+
+  //generic function to draw cells
+  function styleCells(x,y) {
+
+    ctx.fillStyle = "blue";
+    ctx.fillRect(x*cw, y*cw, cw, cw);
+    ctx.strokeStyle = "white";
+    ctx.strokeRect(x*cw, y*cw, cw, cw);
+  };
+
+  function checkCollision(x, y, array){
+    //This function will check if the provided x/y coordinates exist
+    //in an array of cells or not
+    for (var i = 0; i < array.length; i++) {
+      if (array[i].x == x && array[i].y == y)
+        return true;
+    };
+    return false;
+  };
 
   //Lets add the keyboard controls now
 	$(document).keydown(function(e){
@@ -81,9 +145,5 @@ $(document).ready(function () {
 		else if(key == "40" && direction != "up") direction = "down";
 	})
 
-  //Lets move the snake now using a timer which will trigger the paint function
-	//every 300ms for testing
-
- var gameLoop = setInterval(snakeStyle, 300);
 
 });
